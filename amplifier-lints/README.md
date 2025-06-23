8ec07a950dd6c4272372fafdc72eee16b9b5c3e3
Andrew Jia <andrew@interoplabs.io>
2025-06-23 16:42:22 -0400

    Initial Commit
    - Set up scaffolding for amplifier-lints
    - Added READMEs to guide setup and development
    - Created the first lint: warn_on_unwraps, which warns on the unwrap of any Options or Results

---
+++ amplifier-lints/README.md
@@ -0,0 +1,9 @@
+# Amplifier Lints
+
+Lints for the `axelar-amplifier` repository.
+
+## Current Lints
+
+| Lint                                                      | Description                                   |
+| --------------------------------------------------------- | --------------------------------------------- |
+| [`warn_on_unwraps`](./amplifier-lints/warn_on_unwraps)    | Warns on any unwraps used in production code. |
---
+++ amplifier-lints/warn_on_unwraps/src/lib.rs
@@ -0,0 +1,45 @@
+#![feature(rustc_private)]
+#![feature(let_chains)]
+#![warn(unused_extern_crates)]
+
+extern crate rustc_hir;
+extern crate rustc_middle;
+extern crate rustc_span;
+
+use rustc_hir::{Expr, ExprKind};
+use rustc_lint::{LateContext, LateLintPass, LintContext};
+use rustc_middle::ty;
+use rustc_span::sym;
+
+dylint_linting::declare_late_lint! {
+    pub WARN_ON_UNWRAPS,
+    Warn,
+    "warns if `Option::unwrap` or `Result::unwrap` is called"
+}
+
+impl<'tcx> LateLintPass<'tcx> for WarnOnUnwraps {
+    fn check_expr(&mut self, cx: &LateContext<'tcx>, expr: &'tcx Expr<'tcx>) {
+        match expr.kind {
+            ExprKind::MethodCall(name, recv, _args, span) => {
+                if name.ident.as_str() != "unwrap" {
+                    return;
+                }
+                let caller_ty = cx.typeck_results().expr_ty(recv);
+                let is_option_or_result = match caller_ty.kind() {
+                    ty::Adt(adt, _) => {
+                        cx.tcx.is_diagnostic_item(sym::Option, adt.did())
+                            || cx.tcx.is_diagnostic_item(sym::Result, adt.did())
+                    }
+                    _ => false,
+                };
+                if !is_option_or_result {
+                    return;
+                }
+                cx.span_lint(WARN_ON_UNWRAPS, span, |diag| {
+                    diag.primary_message("avoid using `unwrap` if possible");
+                });
+            }
+            _ => {}
+        }
+    }
+}
