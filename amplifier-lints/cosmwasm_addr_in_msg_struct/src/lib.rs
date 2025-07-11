3e29f282933237195bbfd6ef4330952ad4567d5b
Andrew Jia <andrew@interoplabs.io>
2025-07-11 09:51:35 -0400

    initial

--- amplifier-lints/cosmwasm_addr_in_msg_struct/src/lib.rs
+++ amplifier-lints/cosmwasm_addr_in_msg_struct/src/lib.rs
@@ -9,12 +9,12 @@ use rustc_hir::{FieldDef, Item, ItemKind, QPath, TyKind};
 use rustc_lint::{LateContext, LateLintPass, LintContext};
 
 dylint_linting::declare_late_lint! {
-    pub RESTRICT_COSMWASM_ADDR_IN_MSG_STRUCT,
+    pub COSMWASM_ADDR_IN_MSG_STRUCT,
     Warn,
     "avoid using cosmwasm::Addr in msg typesi"
 }
 
-impl<'tcx> LateLintPass<'tcx> for RestrictCosmwasmAddrInMsgStruct {
+impl<'tcx> LateLintPass<'tcx> for CosmwasmAddrInMsgStruct {
     fn check_item(&mut self, cx: &LateContext<'tcx>, item: &'tcx Item<'tcx>) {
         match item.kind {
             ItemKind::Struct(ident, variant_data, _) => {
@@ -29,7 +29,7 @@ impl<'tcx> LateLintPass<'tcx> for RestrictCosmwasmAddrInMsgStruct {
                 let fields = variant_data.fields();
 
                 check_cosmwasm_addr_in_field(cx, fields).then(|| {
-                    cx.span_lint(RESTRICT_COSMWASM_ADDR_IN_MSG_STRUCT, item.span, |diag| {
+                    cx.span_lint(COSMWASM_ADDR_IN_MSG_STRUCT, item.span, |diag| {
                         diag.primary_message("no msg types should use cosmwasm::Addr");
                     });
                 });
@@ -49,7 +49,7 @@ impl<'tcx> LateLintPass<'tcx> for RestrictCosmwasmAddrInMsgStruct {
                     let fields = variant.data.fields();
 
                     check_cosmwasm_addr_in_field(cx, fields).then(|| {
-                        cx.span_lint(RESTRICT_COSMWASM_ADDR_IN_MSG_STRUCT, item.span, |diag| {
+                        cx.span_lint(COSMWASM_ADDR_IN_MSG_STRUCT, item.span, |diag| {
                             diag.primary_message("no msg types should use cosmwasm::Addr");
                         });
                     });
