107d3c7e4a9408f88b91ff6576f5194b70c77989
Andrew Jia <andrew@interoplabs.io>
2025-07-15 14:46:16 -0400

    rename

--- amplifier-lints/README.md
+++ amplifier-lints/README.md
@@ -9,7 +9,11 @@ Lints for the `axelar-amplifier` repository.
 | [`warn_on_unwraps`](./amplifier-lints/warn_on_unwraps) | Warns on any unwraps used in production code. |
 | [`msg_without_explicit_permissions`](./msg_without_explicit_permissions) | Warns on any ExecuteMsg that doesn't derive Permissions. |
 | [`cosmwasm_addr_in_msg_struct`](./cosmwasm_addr_in_msg_struct)| Warns on any msg type with a field of type `cosmwasm_std::Addr` |
+<<<<<<< HEAD
 | [`get_fn`](./amplifier-lints/get_fn) | Warns on any function beginning with `get_`. |
 | [`warn_on_ref_opt_type`](./amplifier-lints/warn_on_ref_opt_type) | Warns on any `&Option<T>`; use `Option<&T>` instead. |
+=======
+| [`ref_opt_type`](./amplifier-lints/ref_opt_type) | Warns on any `&Option<T>`; use `Option<&T>` instead. |
+>>>>>>> b7b6338 (rename)
 
 - TODO: add tests to lints
--- amplifier-lints/ref_opt_type/src/lib.rs
+++ amplifier-lints/ref_opt_type/src/lib.rs
@@ -8,12 +8,12 @@ use rustc_hir::{def::Res, AmbigArg, QPath, Ty, TyKind};
 use rustc_lint::{LateContext, LateLintPass, LintContext};
 
 dylint_linting::declare_late_lint! {
-    pub WARN_ON_REF_OPT_TYPE,
+    pub REF_OPT_TYPE,
     Warn,
     "description goes here"
 }
 
-impl<'tcx> LateLintPass<'tcx> for WarnOnRefOptType {
+impl<'tcx> LateLintPass<'tcx> for RefOptType {
     fn check_ty(&mut self, cx: &LateContext<'tcx>, ty: &'tcx Ty<'tcx, AmbigArg>) {
         if let TyKind::Ref(_, inner_ty) = ty.kind {
             if let TyKind::Path(QPath::Resolved(_, path)) = inner_ty.ty.kind {
@@ -21,7 +21,7 @@ impl<'tcx> LateLintPass<'tcx> for WarnOnRefOptType {
                     let path_str = cx.tcx.def_path_str(def_id);
 
                     if path_str == "std::option::Option" {
-                        cx.span_lint(WARN_ON_REF_OPT_TYPE, ty.span, |diag| {
+                        cx.span_lint(REF_OPT_TYPE, ty.span, |diag| {
                             diag.primary_message("use `Option<&T>` instead");
                         });
                     }
