79e4d8f5e40b7cdc66c74ca3d3178b284a245fac
Andrew Jia <andrew@interoplabs.io>
2025-07-17 15:44:20 -0400

    good

--- amplifier-lints/src/lib.rs
+++ amplifier-lints/src/lib.rs
@@ -9,8 +9,8 @@ extern crate rustc_session;
 #[expect(clippy::no_mangle_with_rust_abi)]
 #[unsafe(no_mangle)]
 pub fn register_lints(sess: &rustc_session::Session, lint_store: &mut rustc_lint::LintStore) {
-    // warn_on_unwraps::register_lints(sess, lint_store);
-    // msg_without_explicit_permissions::register_lints(sess, lint_store);
-    // cosmwasm_addr_in_msg_struct::register_lints(sess, lint_store);
+    warn_on_unwraps::register_lints(sess, lint_store);
+    msg_without_explicit_permissions::register_lints(sess, lint_store);
+    cosmwasm_addr_in_msg_struct::register_lints(sess, lint_store);
     get_fn::register_lints(sess, lint_store);
 }
