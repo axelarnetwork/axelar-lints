305cb37c491b2b0ee524a1ab5b9898402bfaeb2c
Andrew Jia <andrew@interoplabs.io>
2025-07-18 13:56:55 -0400

    rebased

--- amplifier-lints/src/lib.rs
+++ amplifier-lints/src/lib.rs
@@ -10,9 +10,9 @@ extern crate rustc_session;
 #[unsafe(no_mangle)]
 pub fn register_lints(sess: &rustc_session::Session, lint_store: &mut rustc_lint::LintStore) {
     cosmwasm_addr_in_msg_struct::register_lints(sess, lint_store);
+    execute_without_explicit_permissions::register_lints(sess, lint_store);
     get_fn::register_lints(sess, lint_store);
     msg_without_explicit_permissions::register_lints(sess, lint_store);
     ref_opt_type::register_lints(sess, lint_store);
     warn_on_unwraps::register_lints(sess, lint_store);
-    execute_without_explicit_permissions::register_lints(sess, lint_store);
 }
