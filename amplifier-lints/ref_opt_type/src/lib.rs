8bfbf8bed065f506e44d52510edd996ff7b06654
Andrew Jia <andrew@interoplabs.io>
2025-07-18 10:45:33 -0400

    change to give actual type in warning

--- amplifier-lints/ref_opt_type/src/lib.rs
+++ amplifier-lints/ref_opt_type/src/lib.rs
@@ -3,9 +3,11 @@
 #![warn(unused_extern_crates)]
 
 extern crate rustc_hir;
+extern crate rustc_span;
 
-use rustc_hir::{AmbigArg, QPath, Ty, TyKind, def::Res};
+use rustc_hir::{AmbigArg, GenericArg, Path, QPath, Ty, TyKind, def::Res};
 use rustc_lint::{LateContext, LateLintPass, LintContext};
+use rustc_span::Span;
 
 dylint_linting::declare_late_lint! {
     pub REF_OPT_TYPE,
@@ -20,11 +22,29 @@ impl<'tcx> LateLintPass<'tcx> for RefOptType {
             && let Res::Def(_, def_id) = path.res
             && Some(def_id) == cx.tcx.lang_items().option_type()
         {
+            let inner_snippet = get_snippet_or_default(cx, path);
+
             cx.span_lint(REF_OPT_TYPE, ty.span, |diag| {
-                diag.primary_message("use `Option<&T>` instead");
+                diag.primary_message(format!("use `Option<&{}>` instead", inner_snippet));
             });
         }
     }
+}
+
+fn get_snippet_or_default(cx: &LateContext<'_>, path: &Path) -> String {
+    if let Some(seg) = path.segments.last()
+        && let Some(gen_args) = seg.args
+        && let Some(GenericArg::Type(opt_inner_ty)) = gen_args.args.last()
+    {
+        return get_snippet(cx, opt_inner_ty.span).unwrap_or_else(|| "T".to_string());
+    }
+
+    "T".to_string()
+}
+
+fn get_snippet(cx: &LateContext<'_>, span: Span) -> Option<String> {
+    let sess = cx.tcx.sess;
+    sess.source_map().span_to_snippet(span).ok()
 }
 
 #[test]
