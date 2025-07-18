b6e5ed9a2cfba17c31ce6c5a6c023a2b49b0fe37
andrew-interop <andrew@interoplabs.io>
2025-07-18 10:50:45 -0400

    feat(lints): restrict `get_` fn lints to only definitions and trait definitions (#9)

--- amplifier-lints/get_fn/src/lib.rs
+++ amplifier-lints/get_fn/src/lib.rs
@@ -2,29 +2,50 @@
 #![feature(let_chains)]
 #![warn(unused_extern_crates)]
 
-extern crate rustc_ast;
+extern crate rustc_hir;
 extern crate rustc_span;
 
-use rustc_ast::{NodeId, visit::FnKind};
-use rustc_lint::{EarlyContext, EarlyLintPass, LintContext};
-use rustc_span::Span;
+use rustc_hir::{ImplItem, ImplItemKind, Item, ItemKind, TraitItem, TraitItemKind};
+use rustc_lint::{LateContext, LateLintPass, LintContext};
+use rustc_span::Ident;
 
-dylint_linting::declare_early_lint! {
+dylint_linting::declare_late_lint! {
     pub GET_FN,
     Warn,
     "no functions should start with `get_`"
 }
 
-impl EarlyLintPass for GetFn {
-    fn check_fn(&mut self, cx: &EarlyContext, fn_kind: FnKind, span: Span, _: NodeId) {
-        if let FnKind::Fn(_, _, fn_data) = fn_kind {
-            let name = fn_data.ident.name.as_str();
-            if name.starts_with("get_") {
-                cx.span_lint(GET_FN, span, |diag| {
-                    diag.primary_message("getter functions should omit the `get_` prefix");
-                });
-            }
+impl<'tcx> LateLintPass<'tcx> for GetFn {
+    fn check_item(&mut self, cx: &LateContext<'tcx>, item: &'tcx Item<'tcx>) {
+        if let ItemKind::Fn { ident, .. } = item.kind {
+            check_fn_name(cx, ident);
         }
+    }
+
+    fn check_impl_item(&mut self, cx: &LateContext<'tcx>, impl_item: &'tcx ImplItem<'tcx>) {
+        let parent_id = cx.tcx.hir_get_parent_item(impl_item.hir_id());
+        let parent_item = cx.tcx.hir_expect_item(parent_id.def_id);
+        if let ItemKind::Impl(parent_impl_item) = parent_item.kind
+            && parent_impl_item.of_trait.is_none()
+            && matches!(impl_item.kind, ImplItemKind::Fn(..))
+        {
+            check_fn_name(cx, impl_item.ident);
+        }
+    }
+
+    fn check_trait_item(&mut self, cx: &LateContext<'tcx>, trait_item: &'tcx TraitItem<'tcx>) {
+        if matches!(trait_item.kind, TraitItemKind::Fn(..)) {
+            check_fn_name(cx, trait_item.ident);
+        }
+    }
+}
+
+fn check_fn_name(cx: &LateContext<'_>, ident: Ident) {
+    let name = ident.name.as_str();
+    if name.starts_with("get_") {
+        cx.span_lint(GET_FN, ident.span, |diag| {
+            diag.primary_message("getter functions should omit the `get_` prefix");
+        });
     }
 }
 
