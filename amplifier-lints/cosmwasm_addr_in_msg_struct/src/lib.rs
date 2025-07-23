24816c7fa769af60aeca9fef36f9bfad11c6bafc
Andrew Jia <andrew@interoplabs.io>
2025-07-23 13:26:57 -0400

    initial

--- amplifier-lints/cosmwasm_addr_in_msg_struct/src/lib.rs
+++ amplifier-lints/cosmwasm_addr_in_msg_struct/src/lib.rs
@@ -18,9 +18,7 @@ impl<'tcx> LateLintPass<'tcx> for CosmwasmAddrInMsgStruct {
     fn check_item(&mut self, cx: &LateContext<'tcx>, item: &'tcx Item<'tcx>) {
         match item.kind {
             ItemKind::Struct(ident, variant_data, _) => {
-                if ident.name.as_str() != "ExecuteMsg"
-                    && ident.name.as_str() != "InstantiateMsg"
-                    && ident.name.as_str() != "QueryMsg"
+                if ident.name.as_str() != "InstantiateMsg"
                     && ident.name.as_str() != "MigrateMsg"
                 {
                     return;
@@ -36,9 +34,7 @@ impl<'tcx> LateLintPass<'tcx> for CosmwasmAddrInMsgStruct {
             }
             ItemKind::Enum(ident, enum_def, _) => {
                 if ident.name.as_str() != "ExecuteMsg"
-                    && ident.name.as_str() != "InstantiateMsg"
                     && ident.name.as_str() != "QueryMsg"
-                    && ident.name.as_str() != "MigrateMsg"
                 {
                     return;
                 }
